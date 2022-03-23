e435f77ba77692677448b42bc6f710b39c5b3a50
Steven Briggs <Briggs.steven369@gmail.com>
2022-03-23 16:17:19 -0700

    Color Changes

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/srv/http/sixthworldsprawl.com/env"
+set -gx VIRTUAL_ENV "/home/rocin/Git/sixthworldsprawl.com/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
