75bbef640c74690fc00ee815af6616a2cc9ed0af
Steven <Briggs.steven369@gmail.com>
2022-12-07 22:40:22 -0800

    removed unneeded pages

--- sixthworldsprawl/routes/general.py
+++ sixthworldsprawl/routes/general.py
@@ -3,16 +3,18 @@ from flask import render_template, Blueprint  # , redirect, session, request
 general = Blueprint("general", __name__)
 
 
+@general.app_errorhandler(404)
+def custom_error_page(e):
+    return render_template("error.html", title="404 - Page Not Found!")
+
 @general.route("/")
 def index():
     return render_template("index.html", title="Sixth World Sprawl")
 
-
-@general.route("/characters/")
+@general.route("/characters")
 def characters():
-    return render_template("characters.html")
+    return render_template("characters.html", title="Meet The Runners")
 
-
-@general.route("/decrypt/")
-def decrypt():
-    return render_template("decrypt.html")
+@general.route("/roller")
+def roller():
+    return render_template("diceroller.html", title="Dice Roller")
