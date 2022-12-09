b5cfc45bd5bb3f062e75a31fdeab7d555abaab26
Steven <Briggs.steven369@gmail.com>
2022-12-08 18:25:31 -0800

    update models

--- sixthworldsprawl/models.py
+++ sixthworldsprawl/models.py
@@ -5,12 +5,93 @@ db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
-    userid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    password = db.Column(db.String(102))
+    """
+    | id:            The primary key for the user
+    | authenticated: Whether or not the user has logged in.
+    | password:      A password hashed with werkzeug.generate_password_hash
+    | email:         A string containing the user's email address
+    | first_name:     A string containing the user's first name
+    | last_name:     A string containing the user's last name
+    | is_admin:      A boolean determining whether or not the user is an admin
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    authenticated = db.Column(db.Boolean, default=False)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(256), nullable=False)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    is_admin = db.Column(db.Boolean, default=False)
+    
+    def is_authenticated(self):
+        return self.authenticated
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def password(self):
+        return self.password_hash
 
     def get_id(self):
-        return self.userid
+        return self.id
+
+    def hash_password(self, password):
+        """
+        Hashes the password passed in and returns it to the caller.
+
+        | Parameters:
+        |     password: unhashed, cleartext string
+
+        | Returns:
+        |     password: Hashed password
+        """
+
+        password = generate_password_hash(password, method='pbkdf2:sha256',
+                                          salt_length=16)
+
+        return password
+
+    @password.setter
+    def password(self, password):
+        """
+        Updates the password on the user model.
+
+        | Parameters:
+        |     password: unhashed, cleartext string
+
+        | Returns:
+        |     None
+        """
+
+        # password = self.hash_password(password)
+        self.password_hash = self.hash_password(password)
+
+    def check_password(self, password):
+        """
+        Checks to see if the password provided matches the current password
+        for the user.
+
+        Parameters:
+
+            password: unhashed, cleartext string
+
+        Returns:
+
+            None
+        """
+
+        if check_password_hash(self.password_hash, password):
+            return True
+        return False
 
 
 # class Request(db.Model):
