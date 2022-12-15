30e9c1fb03b2748a9fda12e57a26605de01a67e1
Steven <Briggs.steven369@gmail.com>
2022-12-14 18:57:05 -0800

    bugfix -> different modal contamination

--- sixthworldsprawl/static/js/modals.js
+++ sixthworldsprawl/static/js/modals.js
@@ -3,17 +3,12 @@ function openModal(title, pageURL) {
     console.log(pageURL);
 
     setContent(pageURL);
-    $('.modal-title').html(title);
+    $('#genericModalTitle').html(title);
     $('#genericModal').modal('show');
 }
 
 function setContent(pageURL) {
     //First Clear out the exisitng content
-    $('.modal-body').empty();
-
-    $('.modal-body').load(pageURL);
-
-    fetch(pageURL)
-        .then((response) => response.text())
-        .then((text) => ($('.modal-body').inngerHTML = text));
+    $('#genericModalBody').empty();
+    $('#genericModalBody').load(pageURL);
 }
